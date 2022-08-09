1e6d6b3413c3835be89341ffce329e79ce3c342f
Ikuni17 <white.brad17@gmail.com>
2022-08-09 11:51:37 -0600

    Add support for timeout and error handling

--- nitropay/sponsor.py
+++ nitropay/sponsor.py
@@ -1,25 +1,38 @@
+from sqlite3 import Time
 import jwt
 import requests
 from datetime import datetime
+from requests import Timeout
+
 
 class Signer:
     def __init__(self, private_key):
         self.private_key = private_key
 
     def sign(self, user_id, site_id, name="", email="", avatar=""):
-        encoded_jwt = jwt.encode({
-            "iss": site_id,
-            "sub": user_id,
-            "iat": datetime.utcnow(),
-            "name": name,
-            "email": email,
-            "avatar": avatar,
-            }, self.private_key, algorithm="HS512")
+        encoded_jwt = jwt.encode(
+            {
+                "iss": site_id,
+                "sub": user_id,
+                "iat": datetime.utcnow(),
+                "name": name,
+                "email": email,
+                "avatar": avatar,
+            },
+            self.private_key,
+            algorithm="HS512",
+        )
         return encoded_jwt
 
-    def getUserSubscription(self, user_id):
-        r = requests.get(f"https://sponsor-api.nitropay.com/v1/users/{user_id}/subscription", headers={'Authorization': self.private_key})
-        if r.status_code != 200:
+    def getUserSubscription(self, user_id, timeout=5):
+        try:
+            r = requests.get(
+                f"https://sponsor-api.nitropay.com/v1/users/{user_id}/subscription",
+                headers={"Authorization": self.private_key},
+                timeout=timeout,
+            )
+            if r.status_code != 200:
+                return {}
+            return r.json()
+        except Timeout:
             return {}
-        return r.json()
-
